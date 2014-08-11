d8df0cdec56e9e0855f963ba23571d1f9350190d
Kurt Sang Hyun Lee <kurt@vingle.net>
2014-08-12 00:39:24 +0900

    WIP

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require_tree .
+
+//= require_tree ./lib
+
+//= require angular
+//= require angular-touch
+//= require angular-carousel
+//= require_tree ./angular
